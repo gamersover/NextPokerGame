560eeb2712e314ac24d2aedb2c3c3ba687d8b4ac
gamersover <cmathking@gmail.com>
2023-09-03 14:32:19 +0800

    修复localstorage获取key的错误

--- utils/hooks.js
+++ utils/hooks.js
@@ -1,38 +1,38 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 const storage = {
     getItem(key, initialValue) {
-      if (typeof window === "undefined") {
-        return initialValue;
-      }
-      try {
-        const unparsedValue = window.localStorage.getItem(key);
-        if (typeof unparsedValue === "undefined") {
-          return initialValue;
+        if (typeof window === "undefined") {
+            return initialValue;
         }
-        return JSON.parse(unparsedValue);
-      } catch (error) {
-        return initialValue;
-      }
+        try {
+            const unparsedValue = window.localStorage[key];
+            if (typeof unparsedValue === "undefined") {
+                return initialValue;
+            }
+            return JSON.parse(unparsedValue);
+        } catch (error) {
+            return initialValue;
+        }
     },
 
     setItem(key, value) {
-      window.localStorage.setItem(key,  JSON.stringify(value))
+        window.localStorage.setItem(key, JSON.stringify(value))
     },
-  };
+};
 
 
 export function useLocalStorage(key, initialValue) {
     const [value, setValue] = useState(initialValue)
 
     useEffect(() => {
-        setValue(storage.getItem(key))
+        setValue(storage.getItem(key, initialValue))
     }, [key, initialValue])
 
     const setItem = (newValue) => {
         setValue(newValue);
         storage.setItem(key, newValue);
-      };
+    };
 
     return [value, setItem];
 }
